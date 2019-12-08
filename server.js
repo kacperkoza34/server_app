b8ce183c144064efdd31ac95739676294b6c0521
kacperkoza34 <52444354+kacperkoza34@users.noreply.github.com>
2019-12-08 16:04:25 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/index.html'));
 });
 
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname + '/views/index.html'));
+});
+
 app.get('/about', (req, res) => {
   res.sendFile(path.join(__dirname + '/views/about.html'));
 });
